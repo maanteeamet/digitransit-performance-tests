da56ec39a9f85a8b52fc90127947cb346fbe3446
Joel Lappalainen <joel.lappalainen@cgi.com>
2018-05-22 16:26:06 +0300

    add scripts for generating test data

---
+++ test_files/generate_random.py
@@ -0,0 +1,12 @@
+import random
+
+def main():
+    with open('test_data_otp.txt', 'a') as the_file:
+        i = 0
+        while i < 1000:
+            random_row = 'http://opentripplanner-hsl.marathon.l4lb.thisdcos.directory:8080/otp/routers/hsl/index/graphql POST { stop(id: "HSL:' + str(random.randint(1040000, 1050000)) + '") { name lat lon wheelchairBoarding } }'
+            the_file.write(random_row + '\n')
+            i += 1
+
+if __name__ == "__main__":
+    main()
---
+++ test_files/generate_random_pelias.py
@@ -0,0 +1,18 @@
+import random
+
+def main():
+    with open('kunnat') as f:
+        content = f.readlines()
+
+    content = [x.strip() for x in content] 
+    with open('test_data_pelias.txt', 'a') as file1:
+        for line in content:
+            file1.write('http://pelias-api.marathon.l4lb.thisdcos.directory:8080/v1/search?text='+ line + '\n')
+        i = 0
+        while i < 100000:
+            request_url = 'http://pelias-api.marathon.l4lb.thisdcos.directory:8080/v1/search?text=' + content[random.randint(0, 319)] + ',%20' + content[random.randint(0, 319)] + '\n'
+            file1.write(request_url)
+            i += 1
+
+if __name__ == "__main__":
+    main()
